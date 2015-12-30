99ff1452626b329f0b2cbf6beb99448f83bdf4f8
David Westberry <n30r3b3l@gmail.com>
2015-12-30 22:45:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def initialize(name, breed="Mut")
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def breed
+    @breed
+  end
+  def name
+    @name
+  end
+end
